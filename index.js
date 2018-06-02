66bf2e58625ef96e349868cb31f2aa8130588d0f
wingraptor <akonobrathwaite@gmail.com>
2018-06-02 16:14:31 -0400

    First Major Update to page CSS and Content . Also added some more Js functionality
    New image assets were added. All elements were styled including new fonts added.  Found a way to allow elements on Page 3 / Step 3 to Fade in and only fade in after elements from step 2 / Page 2 have faded out.  Links to products on artisanbeards.com were added to product cards, but only when product cards are appended to page 3/step 3

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var stepTwoCardId = "";
 var stepOneCardHTML = "";
 var stepTwoCardHTML = "";
 var numberOfDrops = "";
+var isFaded = true;
 
 
 /* Event bound to beardGrowthCards on Page 1; handler can only be triggered once per click as the
@@ -24,8 +25,11 @@ $(".col__Step-One").on( "click", function(){
 
 function stepOne(){
   $(".col__Step-One").fadeOut(500, function(){
-    $(".col__Step-Two").fadeIn(500);
-    h1Changer.textContent = "Step 2: Select Your Product";
+    if(isFaded === true){
+      $(".col__Step-Two").fadeIn(500);
+      h1Changer.innerHTML = "Step 2: Select Your Product";
+      isFaded = !isFaded;
+    }
   });
   stepTwoBinder();
 }
@@ -40,30 +44,39 @@ function stepOne(){
       stepTwoCardId = this.getAttribute("id");
       var stepTwoCard = document.getElementById(stepTwoCardId);
       stepTwoCardHTML = stepTwoCard.outerHTML;
-      stepThree();
+      $(".col__Step-Two").fadeOut(500, function(){
+        if(isFaded === false){
+          stepThree();
+          isFaded = !isFaded;
+        }
+      });
     });
   }
 
+/*Function below evaluates the ID of the beardGrowthCard clicked in order to determine the numberOfDrops
+of beard oil used. Then this function updates the text of the h1 element and includes the appropriate numberOfDrops of oil. */
 
 function stepTwo(){
   var clickedBeardGrowthCard = stepOneCardId;
   switch(clickedBeardGrowthCard){
     case "one":
-      numberOfDrops = "3-4"
+      numberOfDrops = "<span class=\"heading__top--highlight\">3-4</span>"
       break;
     case "two":
-      numberOfDrops = "4-6"
+      numberOfDrops = "<span class=\"heading__top--highlight\">4-6</span>"
       break;
     case "three":
-      numberOfDrops = "7-10"
+      numberOfDrops = "<span class=\"heading__top--highlight\">7-10</span>"
       break;
     case "four":
-      numberOfDrops = "10+"
+      numberOfDrops = "<span class=\"heading__top--highlight\">10+</span>"
       break;
   }
-  h1Changer.textContent = "Recommended Number of Drops: " + numberOfDrops + " drops";
-  $(".col__Step-Two").fadeOut();
+  h1Changer.innerHTML = "Recommended Number of Drops: " + numberOfDrops + " drops per day";
 }
+
+/*Function below stores the HTML of the elements clicked in stepOne and stepTwo and is able
+to return the appropriate html content dependent on what was clicekd */
 
 function stepThree(){
   var clickedBeardGrowthCard = stepOneCardId;
@@ -71,21 +84,33 @@ function stepThree(){
   var clickedBeardGrowthCardHTML = stepOneCardHTML;
   var clickedProductCardHTML = stepTwoCardHTML;
   // Only execute code in function when Page 3 has been loaded; when Read the Instructions Below! is the heading on page.
-  if(h1Changer.textContent === "Recommended Number of Drops: " + numberOfDrops + " drops"){
+  if(h1Changer.innerHTML === "Recommended Number of Drops: " + numberOfDrops + " drops per day"){
     //The below returns appropriate elements depending on elements clicked on stepOne and stepTwo
     switch(clickedBeardGrowthCard){
       case "one":
+        $(".row__top").fadeOut(0);
         $(".row__top").append(clickedBeardGrowthCardHTML, $("#oneMonth").html(), clickedProductCardHTML);
+        $(".row__top").fadeIn(500);
         break;
       case "two":
+        $(".row__top").fadeOut(0);
         $(".row__top").append(clickedBeardGrowthCardHTML, $("#threeMonths").html(), clickedProductCardHTML);
+        $(".row__top").fadeIn(500);
         break;
       case "three":
+        $(".row__top").fadeOut(0);
         $(".row__top").append(clickedBeardGrowthCardHTML, $("#twelveMonths").html(), clickedProductCardHTML);
+        $(".row__top").fadeIn(500);
         break;
       case "four":
+        $(".row__top").fadeOut(0);
         $(".row__top").append(clickedBeardGrowthCardHTML, $("#caveman").html(), clickedProductCardHTML);
+        $(".row__top").fadeIn(500);
         break;
     }
+    /*remove the class (.hidden) from the btn element thereby allowing it to be displayed on the page.
+    the third element(object) had to be selected because the product cards from step 2 / page 2 still exist
+    in the DOM but have display:none;. they were fadedOut*/
+    $(".btn")[2].classList.remove("hidden");
   }
 }
