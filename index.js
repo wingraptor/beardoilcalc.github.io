8edb295891367dfc11d695f8ffdf6c0f28a6dde3
wingraptor <akonobrathwaite@gmail.com>
2018-06-03 09:55:20 -0400

    Fixed Issues with website responsiveness
    The 4th beard growth card has been adjusted such that on small screens it now appears beneath the first card. Added a back button that appears on Step 2 and Step 3.

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ var stepTwoCardHTML = "";
 var numberOfDrops = "";
 var isFaded = true;
 
-
 /* Event bound to beardGrowthCards on Page 1; handler can only be triggered once per click as the
   handler is unbound on click */
 
@@ -27,7 +26,7 @@ function stepOne(){
   $(".col__Step-One").fadeOut(500, function(){
     if(isFaded === true){
       $(".col__Step-Two").fadeIn(500);
-      h1Changer.innerHTML = "Step 2: Select Your Product";
+      h1Changer.innerHTML = "<a href=\"index.html\"><i class=\"far fa-arrow-alt-circle-left\"></i></a><span class=\"heading__top--highlight\">Step 2:</span> Select Your Product";
       isFaded = !isFaded;
     }
   });
@@ -72,7 +71,7 @@ function stepTwo(){
       numberOfDrops = "<span class=\"heading__top--highlight\">10+</span>"
       break;
   }
-  h1Changer.innerHTML = "Recommended Number of Drops: " + numberOfDrops + " drops per day";
+  h1Changer.innerHTML = "<a href=\"index.html\"><i class=\"far fa-arrow-alt-circle-left\"></i></a>"+ "Suggested Number of Drops: " + numberOfDrops + " drops per day";
 }
 
 /*Function below stores the HTML of the elements clicked in stepOne and stepTwo and is able
@@ -83,34 +82,34 @@ function stepThree(){
   var clickedProductCard = stepTwoCardId;
   var clickedBeardGrowthCardHTML = stepOneCardHTML;
   var clickedProductCardHTML = stepTwoCardHTML;
-  // Only execute code in function when Page 3 has been loaded; when Read the Instructions Below! is the heading on page.
-  if(h1Changer.innerHTML === "Recommended Number of Drops: " + numberOfDrops + " drops per day"){
-    //The below returns appropriate elements depending on elements clicked on stepOne and stepTwo
+  // Only execute code below when text has been added to indicate that step3 content is ready to be added to page; i.e. when functionstepTwo has finished executing
+  if(h1Changer.innerHTML === "<a href=\"index.html\"><i class=\"far fa-arrow-alt-circle-left\"></i></a>"+ "Suggested Number of Drops: " + numberOfDrops + " drops per day"){
+    //The below returns appropriate elements depending on elements clicked on step One and Step Two
     switch(clickedBeardGrowthCard){
       case "one":
-        $(".row__top").fadeOut(0);
-        $(".row__top").append(clickedBeardGrowthCardHTML, $("#oneMonth").html(), clickedProductCardHTML);
-        $(".row__top").fadeIn(500);
+        $(".row__top").attr("style", "display: none;"); // removes content on page such that info can be first appended to it
+        $(".row__top").append(clickedBeardGrowthCardHTML, $("#oneMonth").html(), clickedProductCardHTML); // append elements to page
+        $(".row__top").fadeIn(500); // fadeIn all content including the info appended in previous step.
         break;
       case "two":
-        $(".row__top").fadeOut(0);
+        $(".row__top").attr("style", "display: none;");
         $(".row__top").append(clickedBeardGrowthCardHTML, $("#threeMonths").html(), clickedProductCardHTML);
         $(".row__top").fadeIn(500);
         break;
       case "three":
-        $(".row__top").fadeOut(0);
+        $(".row__top").attr("style", "display: none;");
         $(".row__top").append(clickedBeardGrowthCardHTML, $("#twelveMonths").html(), clickedProductCardHTML);
         $(".row__top").fadeIn(500);
         break;
       case "four":
-        $(".row__top").fadeOut(0);
+        $(".row__top").attr("style", "display: none;");
         $(".row__top").append(clickedBeardGrowthCardHTML, $("#caveman").html(), clickedProductCardHTML);
         $(".row__top").fadeIn(500);
         break;
     }
     /*remove the class (.hidden) from the btn element thereby allowing it to be displayed on the page.
     the third element(object) had to be selected because the product cards from step 2 / page 2 still exist
-    in the DOM but have display:none;. they were fadedOut*/
+    in the DOM but have display:none;. they were "hidden"*/
     $(".btn")[2].classList.remove("hidden");
   }
 }
